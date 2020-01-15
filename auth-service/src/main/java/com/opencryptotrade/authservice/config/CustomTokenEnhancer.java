79ed267f140e3e7f3f9a8525f3b835882d5269b1
Nikolay <develnk@gmail.com>
2020-01-15 03:38:01 +0300

    Token roles from principal user authority

--- auth-service/src/main/java/com/opencryptotrade/authservice/config/CustomTokenEnhancer.java
+++ auth-service/src/main/java/com/opencryptotrade/authservice/config/CustomTokenEnhancer.java
@@ -1,6 +1,7 @@
 package com.opencryptotrade.authservice.config;
 
 import com.opencryptotrade.authservice.domain.User;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.oauth2.common.DefaultOAuth2AccessToken;
 import org.springframework.security.oauth2.common.OAuth2AccessToken;
 import org.springframework.security.oauth2.provider.OAuth2Authentication;
@@ -8,6 +9,7 @@ import org.springframework.security.oauth2.provider.token.TokenEnhancer;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 public class CustomTokenEnhancer implements TokenEnhancer {
 
@@ -16,9 +18,7 @@ public class CustomTokenEnhancer implements TokenEnhancer {
         final Map<String, Object> additionalInfo = new HashMap<>();
         User user = (User) authentication.getPrincipal();
         additionalInfo.put("customized", "true");
-        //additionalInfo.put("role", user.getAuthorities().stream().map(GrantedAuthority::getAuthority).collect(Collectors.toList()));
-        additionalInfo.put("role", user.getRoles());
-
+        additionalInfo.put("role", user.getAuthorities().stream().map(GrantedAuthority::getAuthority).collect(Collectors.toList()));
         ((DefaultOAuth2AccessToken) accessToken).setAdditionalInformation(additionalInfo);
         return accessToken;
     }
