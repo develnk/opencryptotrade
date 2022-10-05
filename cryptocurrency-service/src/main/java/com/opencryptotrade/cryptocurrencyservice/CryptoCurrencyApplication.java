7f7c96cac1d076cded333abb7d7d34b2780c87ba
Nikolay Kuzin <develnk@gmail.com>
2022-10-05 15:13:51 +0300

    Updated dependencies

--- cryptocurrency-service/src/main/java/com/opencryptotrade/cryptocurrencyservice/CryptoCurrencyApplication.java
+++ cryptocurrency-service/src/main/java/com/opencryptotrade/cryptocurrencyservice/CryptoCurrencyApplication.java
@@ -2,12 +2,7 @@ package com.opencryptotrade.cryptocurrencyservice;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
-import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
-import org.springframework.context.annotation.ComponentScan;
 import reactor.core.publisher.Hooks;
 
 @Slf4j
