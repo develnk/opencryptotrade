f6ce464123a3e39ceca8ba79021415b0beb6322c
Nikolay Kuzin <develnk@gmail.com>
2022-11-18 14:35:43 +0300

    Small code refactor

---
+++ commons/src/main/java/com/opencryptotrade/common/user/security/annotation/PreAuthorizeAnyEmployeeAndCustomerRW.java
@@ -0,0 +1,12 @@
+package com.opencryptotrade.common.user.security.annotation;
+
+import org.springframework.security.access.prepost.PreAuthorize;
+import java.lang.annotation.*;
+
+@Target({ElementType.METHOD, ElementType.TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+@Inherited
+@Documented
+@PreAuthorize("hasAnyAuthority('ROLE_SUPER', 'ROLE_ADMIN', 'ROLE_COMPANY_ADMIN', 'ROLE_COMPANY_EMPLOYEE', 'ROLE_CUSTOMER_RW')")
+public @interface PreAuthorizeAnyEmployeeAndCustomerRW {
+}
